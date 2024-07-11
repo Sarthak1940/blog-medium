64714688b13b1e8af9d0e4828b8295a47f7a1a1e
Sarthak Godse <sarthak123godse@gmail.com>
2024-07-11 15:20:44 +0530

    add signout button and spinner

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -1,6 +1,7 @@
-import { Link} from "react-router-dom"
+import { Link, useNavigate} from "react-router-dom"
 
 export const AppBar = () => {
+  const navigate = useNavigate()
   return <div className="shadow-sm flex justify-between px-10 py-4 items-center">
     <Link to={"/blog"}>
       <div className="cursor-pointer">
@@ -9,8 +10,16 @@ export const AppBar = () => {
     </Link>
     <div className="flex gap-3">
       <Link to={"/publish"}>
-        <button type="button" className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"
+        <button className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"
         >New Blog</button></Link>
+
+        <button className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"
+        onClick={() => {
+          localStorage.removeItem("token");
+          navigate("/signin")
+          setTimeout(() => {alert("You have been signed out")}, 400)
+        }}
+        >Sign out</button>
 
       <Profile/>
     </div>  
